9ddc677ab9c3038f5d04526aa29052a28dbb2ab9
LengSword <1030588973@qq.com>
2021-01-23 22:02:06 +0800

    Inital blog

---
+++ theme/components/Utterances.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="comments" ref="comments"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    repo: {
+      type: String,
+      required: true
+    },
+    theme: {
+      type: String,
+      required: false
+    }
+  },
+
+  mounted() {
+    const { repo, theme } = this
+    const attrs = {
+      repo: repo,
+      'issue-term': 'pathname',
+      label: 'comment',
+      theme: theme,
+      crossorigin: 'anonymous'
+    }
+
+    const script = (this.$commentScript = document.createElement('script'))
+    script.async = true
+    script.src = `https://utteranc.es/client.js`
+    for (const key of Object.keys(attrs)) {
+      script.setAttribute(key, attrs[key])
+    }
+    this.$refs.comments.append(script)
+  },
+
+  beforeDestroy() {
+    if (this.$commentScript) {
+      this.$commentScript.parentNode.removeChild(this.$commentScript)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.comments {
+  margin-top: 50px;
+  padding-top: 50px;
+  border-top: 1px dashed var(--border-color);
+}
+</style>
