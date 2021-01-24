6d632cd5af0d80ad21e8a3c4a8d5bf463b7ae2eb
LengSword <1030588973@qq.com>
2021-01-25 02:22:35 +0800

    Add Utterances to page layout

--- theme/layouts/page.vue
+++ theme/layouts/page.vue
@@ -8,16 +8,25 @@
       <div class="post-content">
         <slot name="default"></slot>
       </div>
+
+      <Utterances
+        v-if="page.comments !== false && $themeConfig.utterances"
+        :repo="$themeConfig.utterances.repo"
+        :theme="$themeConfig.utterances.theme"
+      />
+
     </article>
   </Wrap>
 </template>
 
 <script>
 import Wrap from '../components/Wrap.vue'
+import Utterances from '../components/Utterances.vue'
 
 export default {
   components: {
-    Wrap
+    Wrap,
+    Utterances
   },
 
   props: ['page']
